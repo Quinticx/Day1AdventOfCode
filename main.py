14ab9f3b1c431035085bdb7f3926b9697443e215
Brianna <witherellbrianna@gmail.com>
2021-12-02 05:55:33 +0000

    Part 2 completed and correct

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+##### Part 1
+
 # Load input file into Numpy array
 sweeps = np.loadtxt("input.txt", dtype=int)
 
@@ -11,4 +13,17 @@ for i,sample in enumerate(sweeps):
 
 print("Amount of sweeps greater than previous: ", count)
 
+    
+##### Part 2
+count = 0
+for i,sample in enumerate(sweeps):
+    if (i+3) >= np.size(sweeps):
+        break
+    group1 = sample + sweeps[i+1] + sweeps[i+2]
+    group2 = sweeps[i+1] + sweeps[i+2] + sweeps[i+3]
 
+    if group2 > group1:
+        count = count + 1
+        print(count)
+    
+print("Amount of sweep groups greater than previous groups: ", count)
